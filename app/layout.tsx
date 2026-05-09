6cf62232f8a0047842c013f9a7a2c2ac2a90abe7
JSam-M <mr.mjohnsamuel@gmail.com>
2026-05-09 21:32:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{touchAction: 'manipulation'}}>
       <body>{children}</body>
     </html>
   )
